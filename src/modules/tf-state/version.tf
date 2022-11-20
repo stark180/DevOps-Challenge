e5ce9fe03e1098d2983e1996debed989a81d15eb
unknown <kisstark181@gmail.com>
2022-11-20 16:01:27 +0300

    updating tf state locking

--- src/modules/tf-state/version.tf
+++ src/modules/tf-state/version.tf
@@ -1,4 +1,12 @@
 terraform {
+  # backend depends on s3 bucket creation
+  backend "s3" {
+    bucket         = "api-tf-state-backend-ci-cd"
+    key            = "tf-infra/terraform.tfstate"
+    region         = "eu-west-3"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -7,12 +15,4 @@ terraform {
 
   }
 
-   # backend depends on s3 bucket creation
-  backend "s3" {
-    bucket         = "api-tf-state-backend-ci-cd"
-    key            = "tf-infra/terraform.tfstate"
-    region         = "eu-west-3"
-    dynamodb_table = "terraform-state-locking"
-    encrypt        = true
-  }
 }
