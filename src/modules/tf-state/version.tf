c9f4a24cf0a95673b8207acc35f7604dce8ba028
unknown <kisstark181@gmail.com>
2022-11-20 15:58:08 +0300

    creating terraform state file locking

--- src/modules/tf-state/version.tf
+++ src/modules/tf-state/version.tf
@@ -6,4 +6,13 @@ terraform {
     }
 
   }
+
+   # backend depends on s3 bucket creation
+  backend "s3" {
+    bucket         = "api-tf-state-backend-ci-cd"
+    key            = "tf-infra/terraform.tfstate"
+    region         = "eu-west-3"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
 }
