6cdbf5083a4a7567f3e3860afa240a296d2c3d57
unknown <kisstark181@gmail.com>
2022-11-20 16:05:24 +0300

    fixing the backend misplacement issue back to the root

--- src/modules/tf-state/version.tf
+++ src/modules/tf-state/version.tf
@@ -1,13 +1,5 @@
 terraform {
-  # backend depends on s3 bucket creation
-  backend "s3" {
-    bucket         = "api-tf-state-backend-ci-cd"
-    key            = "tf-infra/terraform.tfstate"
-    region         = "eu-west-3"
-    dynamodb_table = "terraform-state-locking"
-    encrypt        = true
-  }
-  required_providers {
+   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
