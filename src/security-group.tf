b501d3fa032a6eafd7886cec5eb33e7b9b9f9853
stark180 <kisstark181@gmail.com>
2022-11-21 22:46:25 +0300

    updating the security group

--- src/security-group.tf
+++ src/security-group.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "api_host" {
         from_port   = 443
         to_port     = 443
         protocol    = "tcp"
-        cidr_blocks = ["10.0.0.0/16"]
+        cidr_blocks = ["0.0.0.0/0"]
         ipv6_cidr_blocks = ["::/0"]
     }
 
@@ -29,7 +29,7 @@ resource "aws_security_group" "api_host" {
         from_port   = 80
         to_port     = 80
         protocol    = "tcp"
-        cidr_blocks = ["10.0.0.0/16"]
+        cidr_blocks = ["0.0.0.0/0"]
         ipv6_cidr_blocks = ["::/0"]
     }
     # creating inbound rule for tcp port 22 to ssh into instance
@@ -38,7 +38,7 @@ resource "aws_security_group" "api_host" {
         from_port   = 22
         to_port     = 22
         protocol    = "tcp"
-        cidr_blocks = ["10.0.0.0/16"]
+        cidr_blocks = ["0.0.0.0/0"]
         ipv6_cidr_blocks = ["::/0"]
     }
     # outbound rule protocal -1 means all and port 0 means all
